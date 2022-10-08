7d0672ff68f7bda558bddbd31ab13c948fcd8441
Its-Tachanks <joshuaew06@gmail.com>
2022-10-08 15:36:58 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/global.css";
-import Layout from "../components/layout.js";
+import Layout from "../components/layout.jsx";
 
 function MyApp({ Component, pageProps }) {
   return (
