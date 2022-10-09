1032be73dbfd1bb8951827bb05592441205f00f7
Its-Tachanks <joshuaew06@gmail.com>
2022-10-09 14:42:15 -0400

    Woops, no script for this here

--- public/scripts/NavbarOpacity.js
+++
@@ -1,5 +0,0 @@
-const navbarElement = document.querySelector("#navbar");
-const titleSection = document.querySelector("#titleSection");
-
-// log top bounding rect of title section
-console.log(titleSection.getBoundingClientRect().top);
