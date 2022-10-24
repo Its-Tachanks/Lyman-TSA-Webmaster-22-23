cb35ae2fce7f5904044b070e8f5888eb2c4aef32
Its-Tachanks <joshuaew06@gmail.com>
2022-10-23 22:38:47 -0400

    MUI added

--- components/parts/Navbar.jsx
+++ components/parts/Navbar.jsx
@@ -1,4 +1,4 @@
-// Next js imports
+// Imports
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -10,6 +10,7 @@ import { useRef, useEffect } from "react";
 import styles from "../../styles/parts/Navbar.module.css";
 
 // Component imports
+import { Tooltip, IconButton, Icon } from "@mui/material";
 
 // Page
 export default function Navbar(props) {
@@ -18,9 +19,9 @@ export default function Navbar(props) {
   useEffect(() => {
     window.addEventListener("scroll", () => {
       //console.log(document.body.getBoundingClientRect().top);
-      document.body.getBoundingClientRect().top <= -100
-        ? navbarRef.current.classList.add(`${styles.navBg}`)
-        : navbarRef.current.classList.remove(`${styles.navBg}`);
+      document.body.getBoundingClientRect().top <= -1
+        ? navbarRef.current.classList.add(`${styles.blackBg}`)
+        : navbarRef.current.classList.remove(`${styles.blackBg}`);
     });
   }, []);
 
@@ -43,7 +44,11 @@ export default function Navbar(props) {
       <div
         className={`container-fluid d-flex justify-content-center align-items-center ${styles.navbarContainer}`}
       >
-        <i className="fa-brands fa-docker"></i>
+        <Tooltip title="test" leaveDelay={100}>
+          <IconButton sx={{ color: "var(--icon-color)" }}>
+            <i className="fa-solid fa-bolt"></i>
+          </IconButton>
+        </Tooltip>
       </div>
     </nav>
   );
